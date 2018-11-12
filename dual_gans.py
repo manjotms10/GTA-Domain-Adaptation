b52f364770d82e3b9dcbcc38e88b0e7252e6ea57
tushardobhal <tushardobhal@gmail.com>
2018-11-11 16:20:36 -0800

    working notebook and python file

--- dual_gans.py
+++ dual_gans.py
@@ -22,6 +22,7 @@ from torch.autograd import Variable
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
+from torchvision.utils import save_image
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -38,7 +39,7 @@ data_root = "/datasets/home/32/232/tdobhal/Project/6_train/images/"
 num_images = 23418
 
 # Batch size during training
-batch_size = 128
+batch_size = 16
 
 # Spatial size of training images. All images will be resized to this size using a transformer.
 image_size = 256
@@ -125,7 +126,7 @@ class DataLoader:
             for i in range(idx.shape[0]):
                 x.append(self.image_loader(images_dir + idx[i]))
                 y.append(self.image_loader(labels_dir + idx[i]))
-            yield torch.stack(torch.from_numpy(x)).to(self.device), torch.stack(torch.from_numpy(y)).to(self.device)
+            yield torch.stack(x), torch.stack(y)
 
 
 # In[4]:
@@ -158,81 +159,97 @@ class Generator(nn.Module):
         
         # input is (nc) x 256 x 256
         self.conv1 = nn.Conv2d(nc, ngf, 4, 2, 1, bias=True)
+        self.lr1 = nn.LeakyReLU(inplace=True)
         # state size. (ngf) x 128 x 128
         self.conv2 = nn.Conv2d(ngf, ngf*2, 4, 2, 1, bias=True)
         self.bn2 = nn.BatchNorm2d(ngf*2)
+        self.lr2 = nn.LeakyReLU(inplace=True)
         # state size. (ngf*2) x 64 x 64
         self.conv3 = nn.Conv2d(ngf*2, ngf*4, 4, 2, 1, bias=True)
         self.bn3 = nn.BatchNorm2d(ngf*4)
+        self.lr3 = nn.LeakyReLU(inplace=True)
         # state size. (ngf*4) x 32 x 32
         self.conv4 = nn.Conv2d(ngf*4, ngf*8, 4, 2, 1, bias=True)
         self.bn4 = nn.BatchNorm2d(ngf*8)
+        self.lr4 = nn.LeakyReLU(inplace=True)
         # state size. (ngf*8) x 16 x 16
         self.conv5 = nn.Conv2d(ngf*8, ngf*8, 4, 2, 1, bias=True)
         self.bn5 = nn.BatchNorm2d(ngf*8)
+        self.lr5 = nn.LeakyReLU(inplace=True)
         # state size. (ngf*8) x 8 x 8
         self.conv6 = nn.Conv2d(ngf*8, ngf*8, 4, 2, 1, bias=True)
         self.bn6 = nn.BatchNorm2d(ngf*8)
+        self.lr6 = nn.LeakyReLU(inplace=True)
         # state size. (ngf*8) x 4 x 4
         self.conv7 = nn.Conv2d(ngf*8, ngf*8, 4, 2, 1, bias=True)
         self.bn7 = nn.BatchNorm2d(ngf*8)
+        self.lr7 = nn.LeakyReLU(inplace=True)
         # state size. (ngf*8) x 2 x 2
         self.conv8 = nn.Conv2d(ngf*8, ngf*8, 4, 2, 1, bias=True)
         self.bn8 = nn.BatchNorm2d(ngf*8)
+        self.r8 = nn.ReLU(inplace=True)
         
         # Transpose Convolutional Layers
         
         # input is (ngf*8) x 1 x 1
         self.tr_conv1 = nn.ConvTranspose2d(ngf*8, ngf*8, 4, 2, 1, bias=True)
         self.tr_bn1 = nn.BatchNorm2d(ngf*8)
+        self.tr_r1 = nn.ReLU(inplace=True)
         # state size. (ngf*8)*2 x 2 x 2
         self.tr_conv2 = nn.ConvTranspose2d((ngf*8)*2, ngf*8, 4, 2, 1, bias=True)
         self.tr_bn2 = nn.BatchNorm2d(ngf*8)
+        self.tr_r2 = nn.ReLU(inplace=True)
         # state size. (ngf*8)*2 x 4 x 4
         self.tr_conv3 = nn.ConvTranspose2d((ngf*8)*2, ngf*8, 4, 2, 1, bias=True)
         self.tr_bn3 = nn.BatchNorm2d(ngf*8)
+        self.tr_r3 = nn.ReLU(inplace=True)
         # state size. (ngf*8)*2 x 8 x 8
         self.tr_conv4 = nn.ConvTranspose2d((ngf*8)*2, ngf*8, 4, 2, 1, bias=True)
         self.tr_bn4 = nn.BatchNorm2d(ngf*8)
+        self.tr_r4 = nn.ReLU(inplace=True)
         # state size. (ngf*8)*2 x 16 x 16
         self.tr_conv5 = nn.ConvTranspose2d((ngf*8)*2, ngf*4, 4, 2, 1, bias=True)
         self.tr_bn5 = nn.BatchNorm2d(ngf*4)
+        self.tr_r5 = nn.ReLU(inplace=True)
         # state size. (ngf*4)*2 x 32 x 32
         self.tr_conv6 = nn.ConvTranspose2d((ngf*4)*2, ngf*2, 4, 2, 1, bias=True)
         self.tr_bn6 = nn.BatchNorm2d(ngf*2)
+        self.tr_r6 = nn.ReLU(inplace=True)
         # state size. (ngf*2)*2 x 64 x 64
         self.tr_conv7 = nn.ConvTranspose2d((ngf*2)*2, ngf, 4, 2, 1, bias=True)
         self.tr_bn7 = nn.BatchNorm2d(ngf)
+        self.tr_r7 = nn.ReLU(inplace=True)
         # state size. (ngf)*2 x 128 x 128
         self.tr_conv8 = nn.ConvTranspose2d((ngf)*2, nc, 4, 2, 1, bias=True)
+        self.out = nn.Tanh()
         # state size. (nc) x 256 x 256
     
     def forward(self, x):
         c1 = self.conv1(x)
-        c2 = self.bn2(self.conv2(nn.LeakyReLU(c1)))
-        c3 = self.bn3(self.conv3(nn.LeakyReLU(c2)))
-        c4 = self.bn4(self.conv4(nn.LeakyReLU(c3)))
-        c5 = self.bn5(self.conv5(nn.LeakyReLU(c4)))
-        c6 = self.bn6(self.conv6(nn.LeakyReLU(c5)))
-        c7 = self.bn7(self.conv7(nn.LeakyReLU(c6)))
-        c8 = self.bn8(self.conv8(nn.LeakyReLU(c7)))
+        c2 = self.bn2(self.conv2(self.lr1(c1)))
+        c3 = self.bn3(self.conv3(self.lr2(c2)))
+        c4 = self.bn4(self.conv4(self.lr3(c3)))
+        c5 = self.bn5(self.conv5(self.lr4(c4)))
+        c6 = self.bn6(self.conv6(self.lr5(c5)))
+        c7 = self.bn7(self.conv7(self.lr6(c6)))
+        c8 = self.bn8(self.conv8(self.lr7(c7)))
         
-        t1 = self.tr_bn1(self.tr_conv1(nn.ReLU(c8)))
-        t1 = torch.cat(t1, c7, axis=1)
-        t2 = self.tr_bn2(self.tr_conv2(nn.ReLU(t1)))
-        t2 = torch.cat(t2, c6, axis=1)
-        t3 = self.tr_bn3(self.tr_conv3(nn.ReLU(t2)))
-        t3 = torch.cat(t3, c5, axis=1)
-        t4 = self.tr_bn4(self.tr_conv4(nn.ReLU(t3)))
-        t4 = torch.cat(t4, c4, axis=1)
-        t5 = self.tr_bn5(self.tr_conv5(nn.ReLU(t4)))
-        t5 = torch.cat(t5, c3, axis=1)
-        t6 = self.tr_bn6(self.tr_conv6(nn.ReLU(t5)))
-        t6 = torch.cat(t6, c2, axis=1)
-        t7 = self.tr_bn7(self.tr_conv7(nn.ReLU(t6)))
-        t7 = torch.cat(t7, c1, axis=1)
-        t8 = self.tr_conv8(nn.ReLU(t7))
-        t8 = nn.Tanh(t8)
+        t1 = self.tr_bn1(self.tr_conv1(self.r8(c8)))
+        t1 = torch.cat((t1, c7), dim=1)
+        t2 = self.tr_bn2(self.tr_conv2(self.tr_r1(t1)))
+        t2 = torch.cat((t2, c6), dim=1)
+        t3 = self.tr_bn3(self.tr_conv3(self.tr_r2(t2)))
+        t3 = torch.cat((t3, c5), dim=1)
+        t4 = self.tr_bn4(self.tr_conv4(self.tr_r3(t3)))
+        t4 = torch.cat((t4, c4), dim=1)
+        t5 = self.tr_bn5(self.tr_conv5(self.tr_r4(t4)))
+        t5 = torch.cat((t5, c3), dim=1)
+        t6 = self.tr_bn6(self.tr_conv6(self.tr_r5(t5)))
+        t6 = torch.cat((t6, c2), dim=1)
+        t7 = self.tr_bn7(self.tr_conv7(self.tr_r6(t6)))
+        t7 = torch.cat((t7, c1), dim=1)
+        t8 = self.tr_conv8(self.tr_r7(t7))
+        t8 = self.out(t8)
         return t8
     
 
@@ -240,8 +257,8 @@ class Generator(nn.Module):
 # In[7]:
 
 
-gen_a = Generator()
-gen_b = Generator()
+gen_a = Generator().to(device)
+gen_b = Generator().to(device)
 
 
 # In[8]:
@@ -250,8 +267,7 @@ gen_b = Generator()
 class Discriminator(nn.Module):
     def __init__(self):
         super(Discriminator, self).__init__()
-        self.ngpu = ngpu
-        self.main = nn.Sequential(
+        self.net = nn.Sequential(
             # input is (nc) x 256 x 256
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=True),
             nn.LeakyReLU(0.2, inplace=True),
@@ -263,8 +279,8 @@ class Discriminator(nn.Module):
             nn.BatchNorm2d(ndf * 2),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. 32 x 32
-            nn.MaxPool2d((2, 2)), 
-            
+            nn.MaxPool2d((2, 2)),
+    
             # state size. (ndf*2) x 16 x 16
             nn.Conv2d(ndf * 2, ndf * 4, 4, 2, 1, bias=True),
             nn.BatchNorm2d(ndf * 4),
@@ -279,22 +295,24 @@ class Discriminator(nn.Module):
             nn.Conv2d(ndf * 8, ndf * 8, 4, 2, 1, bias=True),
             nn.BatchNorm2d(ndf * 8),
             nn.LeakyReLU(0.2, inplace=True),
-            
-            # state size. (ndf*8) x 4 x 4
-            nn.Conv2d(ndf * 8, 1, 4, 1, 0, bias=True),
-            nn.Sigmoid()
-            # state size. (1) x 1 x 1
-        )
-
-    def forward(self, input):
-        return self.main(input)
+            )
+        
+        self.flat = nn.Linear(ndf * 8 * 2 * 2, 1)
+        self.out = nn.Sigmoid()
+        
+    def forward(self, x):
+        x = self.net(x)
+        x = x.view(x.size()[0], -1)
+        x = self.flat(x)
+        x = self.out(x)
+        return x
 
 
 # In[9]:
 
 
-dis_a = Discriminator()
-dis_b = Discriminator()
+dis_a = Discriminator().to(device)
+dis_b = Discriminator().to(device)
 
 
 # In[10]:
@@ -311,7 +329,7 @@ gen_b.apply(weights_init_normal)
 dis_b.apply(weights_init_normal)
 
 
-# In[14]:
+# In[11]:
 
 
 criterion = torch.nn.BCELoss()
@@ -333,32 +351,32 @@ checkpoint_interval = 1
 for epoch in range(num_epochs):
     for i in range(num_images // batch_size):
         x, y = next(data.data_generator())
-        real_a = Variable(x)
-        real_b = Variable(y)      
-        valid = Variable(torch.ones((real_A.size(0), 1)).to(device), requires_grad=False)
-        fake = Variable(torch.zeros((real_A.size(0), 1)).to(device), requires_grad=False)
+        real_a = Variable(x).to(device)
+        real_b = Variable(y).to(device)     
+        valid = Variable(torch.ones((real_a.size(0), 1)), requires_grad=False).to(device)
+        fake = Variable(torch.zeros((real_a.size(0), 1)), requires_grad=False).to(device)
         
         # Training Discriminator A with real_A batch
         optim_dis_a.zero_grad();
-        pred_real_dis_a = dis_a(real_a).view(-1)
+        pred_real_dis_a = dis_a(real_a).view(-1, 1)
         err_real_dis_a = criterion(pred_real_dis_a, valid)
         err_real_dis_a.backward()
         
         # Training Discriminator B with real_B batch
         optim_dis_b.zero_grad();
-        pred_real_dis_b = dis_b(real_b).view(-1)
+        pred_real_dis_b = dis_b(real_b).view(-1, 1)
         err_real_dis_b = criterion(pred_real_dis_b, valid)
         err_real_dis_b.backward()
         
-        # Training Discriminator B with fake_B batch of of Generator A
+        # Training Discriminator B with fake_B batch of Generator A
         fake_b = gen_a(real_a)
-        pred_fake_dis_b = dis_b(fake_b.detach()).view(-1)
+        pred_fake_dis_b = dis_b(fake_b.detach()).view(-1, 1)
         err_fake_dis_b = criterion(pred_fake_dis_b, fake)
         err_fake_dis_b.backward()
         
-        # Training Discriminator A with fake_A batch of of Generator B
+        # Training Discriminator A with fake_A batch of Generator B
         fake_a = gen_b(real_b)
-        pred_fake_dis_a = dis_a(fake_a.detach()).view(-1)
+        pred_fake_dis_a = dis_a(fake_a.detach()).view(-1, 1)
         err_fake_dis_a = criterion(pred_fake_dis_a, fake)
         err_fake_dis_a.backward()
         
@@ -370,14 +388,14 @@ for epoch in range(num_epochs):
         
         # Train and update Generator A based on Discriminator B's prediction
         optim_gen_a.zero_grad()
-        pred_out_dis_b = dis_b(fake_b).view(-1)
+        pred_out_dis_b = dis_b(fake_b).view(-1, 1)
         err_gen_a = criterion(pred_out_dis_b, valid)
         err_gen_a.backward()
         optim_gen_a.step()
         
         # Train and update Generator B based on Discriminator A's prediction
         optim_gen_b.zero_grad()
-        pred_out_dis_a = dis_a(fake_a).view(-1)
+        pred_out_dis_a = dis_a(fake_a).view(-1, 1)
         err_gen_b = criterion(pred_out_dis_a, valid)
         err_gen_b.backward()
         optim_gen_b.step()
