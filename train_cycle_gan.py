1e2833be7065a8afef58be981c5799d95d9bfea1
Anurag Paul <anurag1paul@gmail.com>
2018-11-28 22:26:39 -0800

    saving model after every epoch

--- train_cycle_gan.py
+++ train_cycle_gan.py
@@ -64,6 +64,7 @@ def train_cycle_gan():
 
             if iteration % opt['sample_interval'] == 0:
                 cycle_gan.save_progress(images_prefix, epoch, iteration)
+    cycle_gan.save_progress(images_prefix, opt['n_epochs'], total_batches)
 
 
 if __name__ == "__main__":
