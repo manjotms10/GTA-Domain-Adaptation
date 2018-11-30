0d09e9799075e48c0146e68fd69131b2c5989c8a
harshulgupta2 <harshul.gupta2@gmail.com>
2018-11-29 21:07:09 -0800

    First commit

---
+++ start.sh
@@ -0,0 +1,1 @@
+nohup python Keras_Segmentation_Pytorch.py >> log_Keras_Segmentation_Pytorch.txt &
---
+++ to_python.sh
@@ -0,0 +1,3 @@
+rm Keras_Segmentation_Pytorch.py
+jupyter nbconvert --to script Keras_Segmentation_Pytorch.ipynb
+
