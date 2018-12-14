5fc8d36c0d4ddac2523b59d9a694b22cd071db11
Manjot Singh Bilkhu <manjotms10@gmail.com>
2018-12-14 13:47:24 -0800

    Update get_pretrained_models.sh

--- get_pretrained_models.sh
+++ get_pretrained_models.sh
@@ -1,7 +1,6 @@
 mkdir saved_models
 cd saved_models
-wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/semi_cycle_generator_gta_to_city_16.pth
-wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_semi_generator_b_10.pth
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_semi_generator_b.pth
 wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_cycle_gan_semi_generator_a.pth
 wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_semi_generator_b.pth
 wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_cycle_gan_semi_generator_b.pth
@@ -9,4 +8,4 @@ wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_un_generator_a.pth
 wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_cycle_gan_un_generator_a.pth
 wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_un_generator_b.pth
 wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_cycle_gan_un_generator_b.pth
-wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/un_cycle_generator_gta_to_city_16.pth
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_semantic_generator_b.pth
