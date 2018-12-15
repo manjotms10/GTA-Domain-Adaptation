1b530b35a4c80a62e9640d147f25bdaa12397c2c
Anurag Paul <anurag1paul@gmail.com>
2018-12-14 16:41:58 -0800

    fixing fetching pretrained models

--- get_pretrained_models.sh
+++ get_pretrained_models.sh
@@ -1,6 +1,6 @@
 mkdir saved_models
 cd saved_models
-wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_semi_generator_b.pth
+wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_semi_generator_a.pth
 wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_cycle_gan_semi_generator_a.pth
 wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_dual_gans_semi_generator_b.pth
 wget -c http://acmsweb.ucsd.edu/~mbilkhu/gta/test_cycle_gan_semi_generator_b.pth
